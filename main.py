4be3b7d441541aa2dbee7f660c8d27e856865228
Rejoy Chakraborty <63641754+rejoyc-cs@users.noreply.github.com>
2026-04-05 13:45:52 +0530

    Commit 1
    Implemented filling, smoothing and starting point detection

---
+++ main.py
@@ -0,0 +1,190 @@
+import cv2
+import numpy as np
+import os
+
+
+# -----------------------------
+# PREPROCESSING
+# -----------------------------
+def preprocess(image_path):
+
+    img = cv2.imread(image_path)
+    img = cv2.resize(img,(1024,400))
+
+    if img is None:
+        raise ValueError(f"Cannot read image: {image_path}")
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    bin_img = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,175,55,)
+    return gray, bin_img
+
+
+# -----------------------------
+# LIGHT PROJECTION FILLING
+# -----------------------------
+def light_projection(bin_img, regions=8, fill_value=150):
+
+    h, w = bin_img.shape
+    filled = bin_img.copy()
+
+    region_width = w // regions
+
+    for row in range(h):
+        # LEFT → RIGHT
+        for r in range(regions):
+
+            start = r * region_width
+            end = min((r + 1) * region_width, w)
+
+            segment = bin_img[row, start:end]
+            obstacle = np.where(segment == 0)[0]
+
+            if obstacle.size > 0:
+                stop = obstacle[0]
+                filled[row, start:start + stop] = fill_value
+            else:
+                filled[row, start:end] = fill_value
+
+        # RIGHT → LEFT
+        for r in reversed(range(regions)):
+
+            start = r * region_width
+            end = min((r + 1) * region_width, w)
+
+            segment = bin_img[row, start:end]
+
+            obstacle = np.where(segment == 0)[0]
+
+            if obstacle.size > 0:
+                stop = obstacle[-1]
+                filled[row, start + stop:end] = fill_value
+            else:
+                filled[row, start:end] = fill_value
+
+    return filled
+
+# -----------------------------
+# CONNECTED COMPONENT HEIGHT
+# -----------------------------
+def connected_component_analysis(img):
+    heights = []
+    vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+    mask = (img != 255).astype(np.uint8) * 255
+    contours, _ = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+
+        if w*h < 200:   # remove small noise
+            continue
+        
+        heights.append(h)
+        cv2.rectangle(vis, (x,y), (x+w,y+h), (0,0,255), 2)
+    
+    cc_h_avg = np.mean(heights)
+    cc_h_median = np.median(heights)
+
+    return vis, cc_h_avg, cc_h_median
+
+# -----------------------------
+# SMOOTHING
+# -----------------------------
+def smoothing(img, cc_h_avg):
+    th_smooth = int(cc_h_avg * 0.15)
+
+    h, w = img.shape
+    for col in range(w):
+        column_data = img[:, col]
+        white_pixels = np.where(column_data == 255)[0]
+
+        if len(white_pixels) == 0:
+            continue
+
+        diff = np.diff(white_pixels)
+        starts = np.insert(white_pixels[1:][diff != 1], 0, white_pixels[0])
+        ends = np.append(white_pixels[:-1][diff != 1], white_pixels[-1])
+
+        for t_start, t_end in zip(starts, ends):
+            if t_start == 0 or t_end == h - 1:
+                continue
+
+            if img[t_start-1, col] == 0 or img[t_end+1, col] == 0:
+                continue
+
+            t_diff = t_end - t_start
+            roi = img[t_start:t_end+1, col]
+
+            if t_diff <= th_smooth and (not np.any(roi == 0)):
+                mask = (img[t_start:t_end+1, col] != 150)
+                img[t_start:t_end+1, col][mask] = 150
+
+    return img
+
+# -----------------------------
+# START POINT
+# -----------------------------
+def detect_start_points(img, cc_h_median):
+
+    h, w = img.shape
+
+    window_height = int(0.25 * cc_h_median)
+    density_threshold = 0.05   # 5%
+
+    start_points = []
+
+    y = 0
+
+    while y + window_height < h:
+
+        window = img[y:y+window_height, :]
+
+        # text pixels = 0
+        black_pixels = np.sum(window == 0)
+
+        total_pixels = window_height * w
+        density = black_pixels / total_pixels
+
+        if density > density_threshold:
+
+            yc = y + window_height // 2
+            start_points.append((0, yc))
+
+            # jump to next region
+            y += window_height + int(cc_h_median)
+
+        else:
+            y += 1
+
+    return start_points
+
+def draw_start_points(img, start_points):
+
+    vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+    for x,y in start_points:
+        cv2.circle(vis,(x,y),6,(0,0,255),-1)
+
+    return vis
+
+def main(image_path):
+
+    gray, bin_img = preprocess(image_path)
+
+    filled = light_projection(bin_img)
+
+    bbox_img, cc_h_avg, cc_h_median = connected_component_analysis(gray)
+
+    smoothed = smoothing(filled, cc_h_avg)
+
+    start_points = detect_start_points(gray, cc_h_median)
+
+    vis = draw_start_points(gray, start_points)
+
+    cv2.imshow("Start Points", vis)
+    cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    filename = "sample.tif"
+    main(filename)
